b3592f2c1b0a0cce8c5587c5bdd8f95ed7c011ff
manuel.f.gutierrez1999@gmail.com <manuel.f.gutierrez1999@gmail.com>
2022-11-02 13:36:06 -0500

    second commit

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -1,10 +1,11 @@
-const express = require("express");
+const express = require ("express")
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Service is up and running");
+app.get("/" ,(req,res) => {
+
+    res.send("Service is up and running");
 });
 
-app.listen(8080, () => {
-  console.log("Server is up");
-});
+app.listen(5000, () => {
+    console.log("Server is up")
+})
